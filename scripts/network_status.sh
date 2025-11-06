09069d5196d6d82f5b9a8f22a0a40195fa87d63f
XanderTheRat <saussic0canard@gmail.com>
2025-11-06 09:36:32 +0100

    Retrait des modules inutilisés ( CPU/Mem )

--- scripts/network_status.sh
+++ scripts/network_status.sh
@@ -20,7 +20,7 @@ case "$mode" in
   ssid)
     ssid=$(nmcli -t -f DEVICE,TYPE,STATE,CONNECTION dev | grep ':wifi:connected:' | cut -d: -f4)
     if [ -z "$ssid" ]; then
-      echo "<span foreground='#f6ad55'>⚠ Disconnected</span>"
+      echo "<span foreground='#f87171'>⚠ Disconnected</span>"
     else
       echo "<span foreground='#63b3ed'> $ssid</span>"
     fi
@@ -28,17 +28,13 @@ case "$mode" in
   ipv4)
     ipv4=$(ip -4 addr show | grep 'inet ' | awk '{print $2}' | grep -v '127.0.0.1' | head -n 1)
     if [ -z "$ipv4" ]; then
-      echo "<span foreground='#f6ad55'>❌ No IPv4</span>"
+      echo "<span foreground='#f87171'>❌ No IPv4</span>"
     else
       echo "<span foreground='#a78bfa'>ipv4:</span><span foreground='#63b3ed'> $ipv4</span>"
     fi
     ;;
   ipv6)
     ipv6=$(ip -6 a show up | grep 'inet6' | awk '{print $2}' | tail -n 1)
-    if [ -z "$ipv6" ]; then
-      echo "<span foreground='#f6ad55'>❌ No IPv6</span>"
-    else
-      echo "<span foreground='#fb923c'>ipv6:</span><span foreground='#63b3ed'> $ipv6</span>"
-    fi
+    echo "<span foreground='#fb923c'>ipv6:</span><span foreground='#63b3ed'> $ipv6</span>"
     ;;
 esac
